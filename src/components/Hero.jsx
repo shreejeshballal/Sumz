60d7ebd38077e10d849fd362248a49a70eaf3c5d
Shreejesh J Ballal <shreejeshballal@gmail.com>
2023-09-06 15:35:45 +0530

    Final commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import { logo } from "../assets";
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full pt-3 mb-10">
+        <img className="w-28 object-contain" src={logo} />
+        <button
+          type="button"
+          onClick={() => {
+            window.open("https://github.com/shreejeshballal/Sumz");
+          }}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>{" "}
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
